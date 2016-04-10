8e17b32c02bcdc5d1af143c53d35a3299e47044d
Mamun-dueee <mamunduapece12@gmail.com>
2016-04-11 00:47:14 +0600

    python the hard way 1st prog.

---
+++ ex1.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+print "Hello world!"
+print "Hello again."
+print "I like to writing this."
+print " I like Python. "
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print "# this is number sign."
+print "আমি মামুন "
+#print " Fuck u.
